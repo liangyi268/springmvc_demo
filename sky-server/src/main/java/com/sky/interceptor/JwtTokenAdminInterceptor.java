fb32d0196564321ac3879dffc9606cedbe33c2ac
zhoucai <736361374@qq.com>
2026-04-26 20:06:01 +0800

    员工分页查询业务开发

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -50,6 +50,7 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
             BaseContext.setCurrentId(empId);
             log.info("当前员工id：", empId);
             //3、通过，放行
+
             return true;
         } catch (Exception ex) {
             //4、不通过，响应401状态码
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,9 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -24,4 +27,6 @@ public interface EmployeeMapper {
     "values"+
     "(#{username},#{name},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
+
+    Page<Employee> page(EmployeePageQueryDTO employeePageQueryDTO);
 }
