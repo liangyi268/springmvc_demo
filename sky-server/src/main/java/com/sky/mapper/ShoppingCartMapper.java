606710e33862a520405ee9a0b2b14eb983ecbecf
zhoucai <736361374@qq.com>
2026-05-21 20:35:34 +0800

    添加购物车代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,34 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    /**
+     * 添加购物车数据
+     * @param shoppingCart
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 更新购物车数量
+     * @param shoppingCart
+     * @return
+     **/
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 插入购物车数据
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart (name,user_id, image, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
+    "values (#{name}, #{userId},#{image}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime})")
+    void insert(ShoppingCart shoppingCart);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,90 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Slf4j
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Override
+    public void add(ShoppingCartDTO shoppingCartDTO) {
+        log.info("添加购物车，接收到的DTO: {}", shoppingCartDTO);
+        Long userId = BaseContext.getCurrentId();
+        log.info("当前用户ID: {}", userId);
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        shoppingCart.setUserId(userId);
+
+        log.info("查询购物车列表，查询条件: {}", shoppingCart);
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        log.info("查询结果数量: {}, 结果: {}", list == null ? 0 : list.size(), list);
+
+        if (list != null && list.size() > 0) {
+            ShoppingCart cart = list.get(0);
+            log.info("购物车中已存在该商品，原数量: {}", cart.getNumber());
+            cart.setNumber(cart.getNumber() + 1);
+            shoppingCartMapper.updateNumberById(cart);
+            log.info("更新后数量: {}", cart.getNumber());
+        } else {
+            log.info("购物车中不存在该商品，准备新增");
+            Long dishId = shoppingCartDTO.getDishId();
+            if (dishId != null) {
+                log.info("添加的是菜品，菜品ID: {}", dishId);
+                Dish dish = dishMapper.getById(dishId);
+                log.info("查询到的菜品信息: {}", dish);
+                if (dish == null) {
+                    log.error("菜品不存在，菜品ID: {}", dishId);
+                    throw new RuntimeException("菜品不存在");
+                }
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+                log.info("设置菜品信息 - 名称: {}, 价格: {}, 图片: {}", dish.getName(), dish.getPrice(), dish.getImage());
+            } else {
+                Long setmealId = shoppingCartDTO.getSetmealId();
+                log.info("添加的是套餐，套餐ID: {}", setmealId);
+                Setmeal setmeal = setmealMapper.getById(setmealId);
+                log.info("查询到的套餐信息: {}", setmeal);
+                if (setmeal == null) {
+                    log.error("套餐不存在，套餐ID: {}", setmealId);
+                    throw new RuntimeException("套餐不存在");
+                }
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+                log.info("设置套餐信息 - 名称: {}, 价格: {}, 图片: {}", setmeal.getName(), setmeal.getPrice(), setmeal.getImage());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            log.info("准备插入购物车数据: {}", shoppingCart);
+            shoppingCartMapper.insert(shoppingCart);
+            log.info("购物车数据插入成功");
+        }
+    }
+
+
+}
+
